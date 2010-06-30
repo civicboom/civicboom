8228d9689a7755960307e348c2f92a5ab086cdee
Shish <shish@civicboom.com>
2010-06-30 18:19:26 +0100

    table creation

--- src/civicboom/websetup.py
+++ src/civicboom/websetup.py
@@ -10,5 +10,11 @@ def setup_app(command, conf, vars):
     """Place any commands to setup civicboom here"""
     load_environment(conf.global_conf, conf.local_conf)
 
+    from civicboom.model.meta import Base, Session
+    log.info("Creating tables")
+    Base.metadata.drop_all(checkfirst=True, bind=Session.bind)
+    Base.metadata.create_all(bind=Session.bind)
+    log.info("Successfully setup")
+
     # Create the tables if they don't already exist
     meta.metadata.create_all(bind=meta.engine)
