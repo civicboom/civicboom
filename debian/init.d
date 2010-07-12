7fd4e58d9ef76673aebca5a3047267015c0ce0a3
Shish <shish@civicboom.com>
2010-07-12 13:20:35 +0100

    init script

---
+++ debian/init.d
@@ -0,0 +1,27 @@
+#!/bin/sh -e
+
+. /opt/cb-env/bin/activate
+
+APP=/opt/cb/share/website
+UID=--user www-data --group www-data
+DAE=--daemon --pid-file=/var/run/cb-website.pid --log-file=/var/log/cb-website.log
+
+case "$1" in
+  start)
+    paster serve $UID $DAE $APP/production.ini start
+    ;;
+  stop)
+    paster serve $UID $DAE $APP/production.ini stop
+    ;;
+  restart)
+    paster serve $UID $DAE $APP/production.ini restart
+    ;;
+  force-reload)
+    paster serve $UID $DAE $APP/production.ini restart
+    ;;
+  *)
+    echo "Usage: $0 {start|stop|restart|force-reload}"
+    exit 1
+esac
+
+exit 0
