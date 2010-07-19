a49cb08b659fd1cf4f49c413f4b7e1814b3ae6fd
Shish <shish@civicboom.com>
2010-07-19 15:56:15 +0100

    add misc test bits to MiscController

--- src/civicboom/controllers/misc.py
+++ src/civicboom/controllers/misc.py
@@ -1,5 +1,7 @@
 from civicboom.lib.base import BaseController, render, c
+import logging
 
+user_log = logging.getLogger("user")
 prefix = '/misc/'
 
 class MiscController(BaseController):
@@ -9,3 +11,17 @@ class MiscController(BaseController):
       if c.logged_in_user:
         test_text += " user logged in: " + c.logged_in_user.username
       return test_text #render(prefix + 'contact.mako')
+
+    def fill_log(self):
+        user_log.debug("debug")
+        user_log.info("info")
+        user_log.warning("warning")
+        user_log.error("error")
+        user_log.critical("critical")
+        return 'Log events added'
+
+    def fail(self):
+        return None + None
+
+    def ping(self):
+        return 'pong'
