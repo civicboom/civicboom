2076623d3d1420865c31a654b0715212a70a7b1f
Shish <shish@civicboom.com>
2010-07-19 13:50:49 +0100

    start of event log filtering

--- src/civicboom/controllers/eventlog.py
+++ src/civicboom/controllers/eventlog.py
@@ -11,11 +11,18 @@ log = logging.getLogger(__name__)
 class EventlogController(BaseController):
 
     def index(self):
-        #s = request.params["s"]
-        connection = get_engine().connect()
+        # Old-fashioned SQL building since events aren't part of the
+        # SQLAlchemy model; beware of SQL injection
+        # FIXME: make this do something...
+        wheres = ["1=1", ]
+        if "username" in request.params:
+            wheres.append("username = 'blah'")
+        if "address" in request.params:
+            wheres.append("address = '0.0.0.0'")
 
-        result = connection.execute(
-            "SELECT * FROM events ORDER BY date_sent DESC"
-        )
-        #return "\n<p>".join([", ".join([str(col) for col in row]) for row in result])
+        connection = get_engine().connect()
+        query = "SELECT * FROM events"
+        where = " AND ".join(wheres)
+        order = "ORDER BY date_sent DESC"
+        result = connection.execute(query + where + order)
         return render("eventlog.mako", extra_vars={"events": list(result)})
