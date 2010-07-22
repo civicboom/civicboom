110b8a87774b2f4c0a7ee21080263f986c626823
Shish <shish@civicboom.com>
2010-07-22 14:23:21 +0100

    Basic linking to object, not very good though

--- src/civicboom/lib/helpers.py
+++ src/civicboom/lib/helpers.py
@@ -21,3 +21,11 @@ def username_plus_ip(username, address):
         return address
     return HTML.span(username, title=address)
 
+def link_to_objects(text):
+    matches = re.match("(.*)(Content) #(\d+)(.*)", text)
+    if matches:
+        return HTML.literal(matches.group(1)) + HTML.a(
+            matches.group(2)+" #"+matches.group(3),
+            href="/admin/"+matches.group(2)+"/models/"+matches.group(3)+"/edit"
+        ) + HTML.literal(matches.group(4))
+    return text
