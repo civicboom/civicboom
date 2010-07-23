803b7b10fd873bc744fd30f64ec70a7b2b5a12b2
Shish <shish@civicboom.com>
2010-07-23 16:57:51 +0100

    search controller and templates

---
+++ src/civicboom/controllers/search.py
@@ -0,0 +1,24 @@
+import logging
+
+from pylons import request, response, session, tmpl_context as c, url
+from pylons.controllers.util import abort, redirect
+
+from civicboom.lib.base import BaseController, render
+from civicboom.model.content           import Content, DraftContent
+from civicboom.model.meta              import Session
+from sqlalchemy                        import or_
+
+log = logging.getLogger(__name__)
+tmpl_prefix = '/web/design09'
+
+class SearchController(BaseController):
+    def index(self):
+        # Return a rendered template
+        #return render('/search.mako')
+        # or, return a string
+        return 'Hello World'
+
+    def content(self, id):
+        results = Session.query(Content).filter(or_(Content.title.match(id), Content.content.match(id)))
+        return render(tmpl_prefix+"/search/content.mako", extra_vars={"results":results})
+
