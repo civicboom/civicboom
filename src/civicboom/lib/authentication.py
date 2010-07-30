767f2cee8e835e99c6e99050f3e4da4de237a186
Allan <a.callaghan@civicboom.com>
2010-07-30 12:16:40 +0100

    Tidyed AuthKit functions in preparation of Janrain authntication functions

--- src/civicboom/lib/authentication.py
+++ src/civicboom/lib/authentication.py
@@ -1,15 +1,12 @@
+"""
+Tools used for Authentication of Civicboom users
+"""
+
 # Pyhton package imports
 import hashlib
 
 # Pylons imports
-from pylons.templating import render_mako as render
 from pylons.i18n.translation import _, ungettext
-
-# Authkit imports
-from authkit.permissions import RequestPermission
-from authkit.authorize   import PermissionError, NotAuthenticatedError
-from authkit.authorize   import NotAuthorizedError, middleware
-from authkit.authorize.pylons_adaptors import authorize
 
 # Civicboom imports
 from civicboom.lib.misc   import flash_message
@@ -19,39 +16,59 @@ from civicboom.model.meta import Session
 # Other imports
 from sqlalchemy.orm import join
 
-
 # Logging
 import logging
 user_log = logging.getLogger("user")
 
 
-class ValidIndicoUser(RequestPermission):
+#-------------------------------------------------------------------------------
+# AuthKit
+#-------------------------------------------------------------------------------
+
+# Authkit imports
+from authkit.permissions import RequestPermission
+from authkit.authorize   import PermissionError, NotAuthenticatedError
+from authkit.authorize   import NotAuthorizedError, middleware
+from authkit.authorize.pylons_adaptors import authorize
+
+from pylons.templating  import render_mako as render # for render of the signin page (activated outside of the normal base controler as the middleware intercepts it)
+
+
+
+class ValidCivicboomUser(RequestPermission):
+    """
+    Skeliton class to check for a Civicboom user to be present
+    """
     def __init__(self, accept_empty=False):
         self.accept_empty = accept_empty
     def check(self, app, environ, start_response):
         if 'REMOTE_USER' not in environ:
             raise NotAuthenticatedError('Not Authenticated')
         return app(environ, start_response)
-is_valid_user = ValidIndicoUser()
+is_valid_user = ValidCivicboomUser()
 
 
-# Check if a users username and password are valid
-#   function is pointed too from the development.ini file
 def valid_username_and_password(environ, username, password):
-    def get_username_password(username, password):
+    """
+    Check local db if a users username and password are valid
+    
+      for AuthKit, this function is pointed too from the development.ini file
+      authkit.form.authenticate.function = civicboom.lib.authentication:valid_username_and_password
+    """
+    def get_user_and_check_password(username, password):
         password = hashlib.sha1(password).hexdigest()
         try:
             q = Session.query(User).select_from(join(User, UserLogin, User.login_details))
-            q = q.filter(User.username==username)
-            q = q.filter(User.status=='active')
-            q = q.filter(UserLogin.type=='password')
-            q = q.filter(UserLogin.token==password)
+            q = q.filter(User.username   == username  )
+            q = q.filter(User.status     == 'active'  )
+            q = q.filter(UserLogin.type  == 'password')
+            q = q.filter(UserLogin.token == password  )
             q = q.one()
             return q
         except:
             return None
 
-    user = get_username_password(username, password)
+    user = get_user_and_check_password(username, password)
     if user:
         user_log.info(user)
         return True
@@ -59,9 +76,14 @@ def valid_username_and_password(environ, username, password):
         flash_message(_("Incorrect username and password"))
         return False
 
-
-#http://pylonsbook.com/en/1.1/simplesite-tutorial-part-3.html#styling-the-sign-in-screen
 def render_signin():
+    """
+    AuthKit Render Signin
+    Reference: http://pylonsbook.com/en/1.1/simplesite-tutorial-part-3.html#styling-the-sign-in-screen
+
+      for AuthKit, this function is pointed too from the development.ini file
+      authkit.form.template.obj          = civicboom.lib.authentication:render_signin
+    """
     # Future placeholder for differnt login pages based
     #setup_widget_env()
     #if c.widget_reporter: #if identify_widget_url():
@@ -73,10 +95,13 @@ def render_signin():
     result = result.replace('%', '%%').replace('FORM_ACTION', '%s') #VERY IMPORTANT! because this is executing outside of the normal page framework
     return str(result)
 
-# AllanC - could not get this to work WTF?
-# had these in the ini file
-# authkit.cookie.badcookie.page = false
-# authkit.cookie.badcookie.template.obj = indicofb.lib.auth_permissions:render_badcookie
 def render_badcookie():
+    """
+    Cookie Expired Page
+    had these in the ini file
+      authkit.cookie.badcookie.page         = false
+      authkit.cookie.badcookie.template.obj = indicofb.lib.auth_permissions:render_badcookie
+    (AllanC - could not get this to work WTF?)
+    """
     flash_message(_("Your login has expired please log in again"))
     return redirect_to('/')
