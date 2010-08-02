8da1e089486bbe01f96b6a8e7cfab0fe0f746d92
Shish <shish@civicboom.com>
2010-08-02 15:49:06 +0100

    test logging in

--- src/civicboom/tests/functional/test_user_profile.py
+++ src/civicboom/tests/functional/test_user_profile.py
@@ -7,4 +7,30 @@ class TestUserProfileController(TestController):
         assert "Mr U. Test (unittest)" in response
 
     def test_edit(self):
-        response = self.app.get(url(controller='user_profile', action='edit', id='unittest')) # not logged in
+        response = self.app.get(url(controller='user_profile', action='edit', id='unittest'))
+        # FIXME: not logged in, expect login prompt
+
+        response = self.app.post(
+            "/account/signin", #url(controller='account', action='signin'),
+            params={
+                'username': u'unittest',
+                'password': u'password'
+            },
+            extra_environ={
+                'REMOTE_ADDR': '127.0.0.1'
+            }
+        )
+        # FIXME: response has no c?
+        #assert response.c.logged_in_user
+
+        response = self.app.get(url(controller='user_profile', action='edit'))
+        # FIXME: test that with no ID, we get our own user page
+
+        response = self.app.get(url(controller='user_profile', action='edit', id='unittest'))
+        assert "Display name" in response
+
+        response = self.app.get(url(controller='user_profile', action='edit', id='unitfriend'))
+        # FIXME: test that we can't edit other people's profiles
+
+        response = self.app.get(url(controller='user_profile', action='edit', id='MrNotExists'))
+        # FIXME: test for a sensible error when accessing a non-existent profile
