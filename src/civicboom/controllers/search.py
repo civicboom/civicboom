1ea409e823709c875584a5fc6c7ec9f5c73aa065
Shish <shish@civicboom.com>
2010-08-04 11:27:23 +0100

    start of member autocomplete box

--- src/civicboom/controllers/search.py
+++ src/civicboom/controllers/search.py
@@ -4,11 +4,11 @@ import json
 from pylons import request, response, session, tmpl_context as c, url
 from pylons.controllers.util import abort, redirect
 
-from civicboom.lib.base import BaseController, render
-from civicboom.lib.gis  import get_engine
-from civicboom.model.content           import Content, DraftContent
-from civicboom.model.meta              import Session
-from sqlalchemy                        import or_
+from civicboom.lib.base   import BaseController, render
+from civicboom.lib.gis    import get_engine
+from civicboom.model      import Content, Member
+from civicboom.model.meta import Session
+from sqlalchemy           import or_
 
 log = logging.getLogger(__name__)
 tmpl_prefix = '/web/design09'
@@ -46,4 +46,19 @@ class SearchController(BaseController):
             return render(tmpl_prefix+"/search/location.mako")
         elif format == "json":
             json_rows = [{"name":row.name,"location":row.location,"type":row.type} for row in result]
+            return json.dumps({"ResultSet": {"Results": json_rows}})
+
+    def member(self, format="html"):
+        if "query" in request.GET:
+            s = request.GET["query"]
+            q = Session.query(Member)
+            q = q.filter(or_(Member.name.match(s), Member.username.match(s)))
+            result = q[0:20]
+        else:
+            result = []
+
+        if format == "html":
+            return render(tmpl_prefix+"/search/member.mako")
+        elif format == "json":
+            json_rows = [{"username":row.username,"name":row.name,"type":row.__type__} for row in result]
             return json.dumps({"ResultSet": {"Results": json_rows}})
