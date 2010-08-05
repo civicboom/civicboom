e4cbd565f9a1d02571c976b8fc3860ccf1164f50
Shish <shish@civicboom.com>
2010-08-05 18:35:16 +0100

    url() is not defined yet, move it from the module into the function

--- src/civicboom/lib/civicboom_lib.py
+++ src/civicboom/lib/civicboom_lib.py
@@ -7,8 +7,8 @@ from pylons import url
 #-------------------------------------------------------------------------------
 # Users in pending status are forced to complete the registration process.
 #   some urls have to be made avalable to pending users (such as signout, etc)
-pending_user_allowed_list = [url(controller='register', action='new_user'),url(controller='account', action='signout')]
 def deny_pending_user(url_to_check):
+    pending_user_allowed_list = [url(controller='register', action='new_user'),url(controller='account', action='signout')]
     for url_safe in pending_user_allowed_list:
         if url_to_check.find(url_safe)>=0:
             return False
