e814489aa334595fee24e6ef4e9c49830c639004
Allan <a.callaghan@civicboom.com>
2010-08-05 18:41:14 +0100

    removed need for url import in civicboom_lib.py by using substrings

--- src/civicboom/lib/civicboom_lib.py
+++ src/civicboom/lib/civicboom_lib.py
@@ -2,13 +2,13 @@
 Set of helpers specificly to the Civicboom project
   (these are not part of misc because misc is more genereal functions that could be used in a range of projects)
 """
-from pylons import url
+
 
 #-------------------------------------------------------------------------------
 # Users in pending status are forced to complete the registration process.
 #   some urls have to be made avalable to pending users (such as signout, etc)
 def deny_pending_user(url_to_check):
-    pending_user_allowed_list = [url(controller='register', action='new_user'),url(controller='account', action='signout')]
+    pending_user_allowed_list = ['register/new_user','account/signout']
     for url_safe in pending_user_allowed_list:
         if url_to_check.find(url_safe)>=0:
             return False
