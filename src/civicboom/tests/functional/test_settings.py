fde7d1801c0415d7bc130ac77f2b24b5ec4b1515
Shish <shish@civicboom.com>
2010-08-17 14:52:09 +0100

    test things that require logins

--- src/civicboom/tests/functional/test_settings.py
+++ src/civicboom/tests/functional/test_settings.py
@@ -1,31 +1,35 @@
 from civicboom.tests import *
 
 class TestSettingsController(TestController):
-    def test_general(self):
-        # not logged in, expect login prompt
-        response = self.app.get(url(controller='settings', action='save_general', id='unittest'))
-        # FIXME: test it
-
-        return # FIXME: POST requires HTTPS, which we don't have...
-
+    def setUp(self):
         response = self.app.post(
             url(controller='account', action='signin'),
+            extra_environ={'HTTP_X_URL_SCHEME': 'https'},
             params={
                 'username': u'unittest',
                 'password': u'password'
             }
         )
-        # FIXME: response has no c?
-        #assert response.c.logged_in_user
 
+    def test_need_signin(self):
+        response = self.app.get(url(controller='account', action='signout'))
+        response = self.app.get(url(controller='settings', action='general', id='unittest'), status=302)
+        # FIXME: follow the redirect, then:
+        # assert "Sign in" in response
+
+    def test_general(self):
         # test that with no ID, we get our own user page
-        response = self.app.get(url(controller='settings', action='save_general'))
-        # FIXME: test it
+        response = self.app.get(url(controller='settings', action='general'))
+        assert "Display name" in response
 
-        response = self.app.get(url(controller='settings', action='save_general', id='unittest'))
-        # FIXME: this fails? :|
-        # assert "Display name" in response
+        response = self.app.post(
+            url(controller='settings', action='save_general', id='unittest'),
+            params={
+                '_authentication_token': response.session['_authentication_token']
+            }
+        )
 
+    def test_csrf(self):
         # test that a setting without CSRF protection is rejected
         response = self.app.post(
             url(controller='settings', action='save_general'),
@@ -39,6 +43,14 @@ class TestSettingsController(TestController):
         # FIXME: check that we're redirected back to the settings page
         # FIXME: check that the new settings appear on the new page
 
+    def test_edit_other_person_fails(self):
+        response = self.app.get(url(controller='settings', action='general'))
+
         # test that we can't edit other people's profiles
-        response = self.app.get(url(controller='settings', action='save_general', id='unitfriend'))
+        # FIXME: for now, the 'id' paramater is ignored
+        #response = self.app.post(
+        #    url(controller='settings', action='save_general', id='unitfriend'),
+        #    params={'_authentication_token': response.session['_authentication_token']},
+        #    status=403,
+        #)
         # FIXME: test
