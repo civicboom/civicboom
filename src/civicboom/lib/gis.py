777affc799388c979c28b10712cb4498911f0cd4
Shish <shish@civicboom.com>
2010-08-25 15:24:21 +0100

    get_location_by_name implemented with google maps, but turned off until we have a license, see issue #47

--- src/civicboom/lib/gis.py
+++ src/civicboom/lib/gis.py
@@ -9,9 +9,30 @@ import logging
 
 gis_engine = None
 
+# NOTE: use of the google geocoding and other APIs require that the results
+# are displayed using google maps, using google maps on a for-profit website
+# requires a commercial license
+we_have_a_gmaps_license = False
+
 def get_engine():
     global gis_engine
     if not gis_engine:
         gis_engine = engine_from_config(config, 'sqlalchemy.gis.')
     return gis_engine
 
+def get_location_by_name(name):
+    if we_have_a_gmaps_license:
+        import urllib2, json
+        log.debug("Looking up location for %s" % addr)
+        data = urllib2.urlopen("http://maps.google.com/maps/api/geocode/json?sensor=false&address=%s" % addr).read()
+        j = json.loads(data)
+        if j["status"] == "OK":
+            loc = j["results"][0]["geometry"]["location"]
+            return (loc["lng"], loc["lat"])
+        elif j["status"] == "ZERO_RESULTS":
+            return None
+        else:
+            log.error("Error geocoding %s: %s" % (addr, j["status"]))
+            return None
+
+    return None
