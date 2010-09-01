b4b55e777f7bcda369075423c4169bc447e6a557
Shish <shish@civicboom.com>
2010-09-01 15:43:04 +0100

    jquery autocomplete uses term= rather than query=

--- src/civicboom/tests/functional/test_search.py
+++ src/civicboom/tests/functional/test_search.py
@@ -57,12 +57,12 @@ class TestSearchController(TestController):
         response = self.app.get(url(controller='search', action='location'))
 
     def test_location_results(self):
-        response = self.app.get(url(controller='search', action='location', format="json", query="cant"))
+        response = self.app.get(url(controller='search', action='location', format="json", term="cant"))
         # FIXME: test install has no geolocation data
         #assert "Canterbury" in response
 
     def test_location_no_results(self):
-        response = self.app.get(url(controller='search', action='location', format="json", query="waffleville"))
+        response = self.app.get(url(controller='search', action='location', format="json", term="waffleville"))
         assert "Canterbury" not in response
 
 
