6b94723f691d2e7ad609544cedcc9b8f172f39d4
Shish <shish@civicboom.com>
2010-09-07 17:12:01 +0100

    test that settings work over HTTPs

--- src/civicboom/tests/functional/test_settings.py
+++ src/civicboom/tests/functional/test_settings.py
@@ -15,6 +15,7 @@ class TestSettingsController(TestController):
 
         response = self.app.post(
             url(controller='settings', action='save_general', id='unittest'),
+            extra_environ={'HTTP_X_URL_SCHEME': 'https'},
             params={
                 '_authentication_token': self.auth_token
             }
@@ -90,6 +91,7 @@ class TestSettingsController(TestController):
         # test that a setting without CSRF protection is rejected
         response = self.app.post(
             url(controller='settings', action='save_general'),
+            extra_environ={'HTTP_X_URL_SCHEME': 'https'},
             params={
                 'email': u'waffle@iamtesting.com',
             },
