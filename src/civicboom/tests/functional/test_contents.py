c4f718b498aed9492a1a4fac09aa667f69c264e8
Shish <shish@civicboom.com>
2010-09-09 14:05:57 +0100

    more content testing

--- src/civicboom/tests/functional/test_contents.py
+++ src/civicboom/tests/functional/test_contents.py
@@ -2,12 +2,42 @@ from civicboom.tests import *
 
 class TestContentsController(TestController):
 
+    ## index -> show #########################################################
+
     def test_index(self):
         response = self.app.get(url('contents'))
         # Test response...
 
     def test_index_as_xml(self):
         response = self.app.get(url('formatted_contents', format='xml'))
+
+    def test_show(self):
+        response = self.app.get(url('content', id=1))
+
+    def test_show_article(self):
+        response = self.app.get(url('content', id=2))
+        # 2 just happens to be the ID of an Article, so we can test the view counter
+
+    def test_show_non_exist(self):
+        response = self.app.get(url('content', id=0))
+
+    def test_show_comment(self):
+        # comments should not be shown individually -- or should they? With threaded comments,
+        # linking to a subthread may be useful
+        response = self.app.get(url('content', id=3), status=404)
+
+    def test_show_as_xml(self):
+        response = self.app.get(url('formatted_content', id=1, format='xml'))
+
+
+
+    ## new -> create #########################################################
+
+    def test_new(self):
+        response = self.app.get(url('new_content'))
+
+    def test_new_as_xml(self):
+        response = self.app.get(url('formatted_new_content', format='xml'))
 
     def test_create(self):
         response = self.app.post(
@@ -21,11 +51,16 @@ class TestContentsController(TestController):
             }
         )
 
-    def test_new(self):
-        response = self.app.get(url('new_content'))
+    ## edit -> update ########################################################
 
-    def test_new_as_xml(self):
-        response = self.app.get(url('formatted_new_content', format='xml'))
+    def test_edit(self):
+        response = self.app.get(url('edit_content', id=2))
+
+    def test_edit_no_perm(self):
+        response = self.app.get(url('edit_content', id=1), status=401)
+
+    def test_edit_as_xml(self):
+        response = self.app.get(url('formatted_edit_content', id=2, format='xml'))
 
     def test_update(self):
         response = self.app.put(
@@ -44,6 +79,7 @@ class TestContentsController(TestController):
             }
         )
 
+    ## delete ################################################################
     def test_delete(self):
         response = self.app.delete(
             url('content', id=1),
@@ -60,26 +96,3 @@ class TestContentsController(TestController):
                 '_authentication_token': self.auth_token
             }
         )
-
-    def test_show(self):
-        response = self.app.get(url('content', id=1))
-
-    def test_show_article(self):
-        response = self.app.get(url('content', id=2))
-        # 2 just happens to be the ID of an Article, so we can test the view counter
-
-    def test_show_non_exist(self):
-        response = self.app.get(url('content', id=0))
-
-    def test_show_as_xml(self):
-        response = self.app.get(url('formatted_content', id=1, format='xml'))
-
-    def test_edit(self):
-        response = self.app.get(url('edit_content', id=2))
-
-    def test_edit_no_perm(self):
-        response = self.app.get(url('edit_content', id=1), status=401)
-
-    def test_edit_as_xml(self):
-        response = self.app.get(url('formatted_edit_content', id=2, format='xml'))
-
