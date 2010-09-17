3d13b6d37c44a868e3be8f909273b4d4efa106d0
Shish <shish@civicboom.com>
2010-09-17 12:51:55 +0100

    make the model work

--- src/civicboom/model/feed.py
+++ src/civicboom/model/feed.py
@@ -2,12 +2,11 @@
 from civicboom.model.meta import Base
 
 from sqlalchemy import Column, ForeignKey
-from sqlalchemy import Unicode, Integer, PickleType
+from sqlalchemy import UnicodeText, Integer, PickleType
 
 class Feed(Base):
     __tablename__ = "feeds"
     id            = Column(Integer(),     primary_key=True)
     member_id     = Column(Integer(),     ForeignKey('member.id') , nullable=True )
-    timestamp     = Column(DateTime(),    nullable=False, default=func.now())
     query         = Column(PickleType(),  nullable=False)
     comment       = Column(UnicodeText(), nullable=True, doc="optional should the user want to add additional details")
