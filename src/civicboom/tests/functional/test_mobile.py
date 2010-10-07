062f35f5aaa5c8dbd82f7beb6412594fa444261c
Shish <shish@civicboom.com>
2010-10-07 14:02:39 +0100

    mobile controller gets smaller

--- src/civicboom/tests/functional/test_mobile.py
+++ src/civicboom/tests/functional/test_mobile.py
@@ -5,7 +5,7 @@ class TestMobileController(TestController):
     def test_latest_version(self):
         response = self.app.get(url(controller='mobile', action='latest_version'))
 
-    def test_upload(self):
+    def test_media(self):
         #response = self.app.get(url(controller='mobile', action='upload'))
         #response = self.app.get(url(controller='mobile', action='upload_file'))
         pass
