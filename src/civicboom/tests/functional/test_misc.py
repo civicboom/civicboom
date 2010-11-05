f9246b280b508c0897de5a35e1b0c45a14aae99a
Shish <shish@civicboom.com>
2010-11-05 08:39:41 +0000

    test a page that works

--- src/civicboom/tests/functional/test_misc.py
+++ src/civicboom/tests/functional/test_misc.py
@@ -55,28 +55,28 @@ class TestMiscController(TestController):
     def test_mobile_detection(self):
         # test non-mobile
         response = self.app.get(
-            url(controller='misc', action='index'),
+            url(controller='misc', action='titlepage'),
             extra_environ={'HTTP_USER_AGENT': 'Mozilla/5.0 (Windows; U; Windows NT 5.1; fi; rv:1.9.2.8) Gecko/20100722 Firefox/3.6.8'}
         )
         # FIXME: test environ['is_mobile']
 
         # test mobile type 1
         response = self.app.get(
-            url(controller='misc', action='index'),
+            url(controller='misc', action='titlepage'),
             extra_environ={'HTTP_USER_AGENT': 'Mozilla/5.0 (iPhone; U; CPU iPhone OS 3_1_2 like Mac OS X; fr-fr) AppleWebKit/528.18 (KHTML, like Gecko) Mobile/7D11'}
         )
         # FIXME: test environ['is_mobile']
 
         # test mobile type 2
         response = self.app.get(
-            url(controller='misc', action='index'),
+            url(controller='misc', action='titlepage'),
             extra_environ={'HTTP_USER_AGENT': 'w3c asdfasdf'} # tests first 4 characters
         )
         # FIXME: test environ['is_mobile']
 
         # test http_accept
         response = self.app.get(
-            url(controller='misc', action='index'),
+            url(controller='misc', action='titlepage'),
             extra_environ={
                 'HTTP_USER_AGENT': 'asdfasdf', # http_accept isn't tested without user_agent...
                 'HTTP_ACCEPT': 'application/vnd.wap.xhtml+xml'
