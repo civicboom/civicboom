8499a9dbba67565847ca75e43592ce1f2c440f7d
Shish <shish@civicboom.com>
2010-11-30 12:24:01 +0000

    manually base64 encoding is an ugly hack -- we should only accept HTTP standard uploads, so only test HTTP standard uploads

--- src/civicboom/tests/functional/test_mobile.py
+++ src/civicboom/tests/functional/test_mobile.py
@@ -9,7 +9,7 @@ class TestMobileController(TestController):
         response = self.app.get(url(controller='mobile', action='latest_version'))
 
 
-    def test_media_raw(self):
+    def test_media(self):
         self.png1x1 = b64decode('iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAAAAAA6fptVAAAAAXNSR0IArs4c6QAAAApJREFUCNdj+AcAAQAA/8I+2MAAAAAASUVORK5CYII=')
 
         response = self.app.post(
@@ -39,8 +39,8 @@ class TestMobileController(TestController):
             params={
                 '_authentication_token': self.auth_token,
                 'content_id': self.my_article_id,
-                'file_data': self.png1x1[0:30]
             },
+            upload_files = [("file_data", "1x1.png", self.png1x1[0:30])],
             status=201
         )
         response = self.app.post(
@@ -48,8 +48,8 @@ class TestMobileController(TestController):
             params={
                 '_authentication_token': self.auth_token,
                 'content_id': self.my_article_id,
-                'file_data': self.png1x1[30:60]
             },
+            upload_files = [("file_data", "1x1.png", self.png1x1[30:60])],
             status=201
         )
         response = self.app.post(
@@ -57,8 +57,8 @@ class TestMobileController(TestController):
             params={
                 '_authentication_token': self.auth_token,
                 'content_id': self.my_article_id,
-                'file_data': self.png1x1[60:]
             },
+            upload_files = [("file_data", "1x1.png", self.png1x1[60:])],
             status=201
         )
         response = self.app.post(
@@ -70,72 +70,6 @@ class TestMobileController(TestController):
             },
             status=201
         )
-
-    def test_media_base64(self):
-        self.png1x1 = b64decode('iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAAAAAA6fptVAAAAAXNSR0IArs4c6QAAAApJREFUCNdjYAIAAAQAA0VqAKYAAAAASUVORK5CYII=')
-
-        response = self.app.post(
-            url('contents', format="json"),
-            params={
-                '_authentication_token': self.auth_token,
-                'title': "mobile upload test",
-                'type': "article",
-                'content': "upload test article",
-                'license': 'CC-BY',
-                'location': "1.0707 51.2999",
-            },
-            status=201
-        )
-        self.my_article_id = json.loads(response.body)["data"]["id"]
-
-        response = self.app.post(
-            url(controller='mobile', action='media_init', format="json"),
-            params={
-                '_authentication_token': self.auth_token,
-                'content_id': self.my_article_id,
-            },
-            status=201
-        )
-        response = self.app.post(
-            url(controller='mobile', action='media_part', format="json"),
-            params={
-                '_authentication_token': self.auth_token,
-                'content_id': self.my_article_id,
-                'encoding': "base64",
-                'file_data': b64encode(self.png1x1[0:30])
-            },
-            status=201
-        )
-        response = self.app.post(
-            url(controller='mobile', action='media_part', format="json"),
-            params={
-                '_authentication_token': self.auth_token,
-                'content_id': self.my_article_id,
-                'encoding': "base64",
-                'file_data': b64encode(self.png1x1[30:60])
-            },
-            status=201
-        )
-        response = self.app.post(
-            url(controller='mobile', action='media_part', format="json"),
-            params={
-                '_authentication_token': self.auth_token,
-                'content_id': self.my_article_id,
-                'encoding': "base64",
-                'file_data': b64encode(self.png1x1[60:])
-            },
-            status=201
-        )
-        response = self.app.post(
-            url(controller='mobile', action='media_finish', format="json"),
-            params={
-                '_authentication_token': self.auth_token,
-                'content_id': self.my_article_id,
-                'file_name': 'test.png',
-            },
-            status=201
-        )
-
 
     def test_error(self):
         response = self.app.get(url(controller='mobile', action='error', format="json"))
