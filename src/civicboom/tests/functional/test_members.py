6e5a45265bc3552746fcf0cab3b8410e0b50536e
Shish <shish@civicboom.com>
2010-12-14 15:42:48 +0000

    follow / unfollow tests

--- src/civicboom/tests/functional/test_members.py
+++ src/civicboom/tests/functional/test_members.py
@@ -5,29 +5,59 @@ class TestMembersController(TestController):
     def test_member_page(self):
         response = self.app.get(url('members', format='json'))
 
-    def test_member_name_results(self):
+    def test_member_list(self):
+        # by name
         response = self.app.get(url('members', format="json", list="all", term="mr"))
         assert "Mr U. Test" in response
 
-    def test_member_username_results(self):
+        # by username
         response = self.app.get(url('members', format="json", list="all", term="unit"))
         assert "Mr U. Test" in response
 
-    def test_member_no_results(self):
+        # invalid
         response = self.app.get(url('members', format="json", list="all", term="waffleville"))
         assert "Mr" not in response
-
 
     def test_member_show(self):
         response = self.app.get(url('member', id='unittest', format='json'))
 
-    def test_member_show_nonexist(self):
+        # non-existent members should 404
         response = self.app.get(url('member', id='mrdoesnotexist', format='json'), status=404)
 
-    def test_member_show_content_list(self):
+        # show content from members
         response = self.app.get(url('member_action', id='unittest', action='content', format='json'))
         response = self.app.get(url('member_action', id='unittest', action='content', list='articles', format='json'))
 
-    def test_member_show_bad_content_list(self):
+        # badly named content lists should give "bad paramaters" error
         response = self.app.get(url('member_action', id='unittest', action='content', list='cake', format='json'), status=400)
+
+    def test_member_follow(self):
+        # no following self
+        response = self.app.post(
+            url('member_action', id='unittest', action='follow', format='json'),
+            params={'_authentication_token': self.auth_token},
+            status=400
+        )
+
+        # can follow someone else; refollow is error?
+        response = self.app.post(
+            url('member_action', id='mobiletest', action='follow', format='json'),
+            params={'_authentication_token': self.auth_token},
+        )
+        response = self.app.post(
+            url('member_action', id='mobiletest', action='follow', format='json'),
+            params={'_authentication_token': self.auth_token},
+            status=400
+        )
+
+        # can unfollow a followed person; re-unfollow is error?
+        response = self.app.post(
+            url('member_action', id='mobiletest', action='unfollow', format='json'),
+            params={'_authentication_token': self.auth_token},
+        )
+        response = self.app.post(
+            url('member_action', id='mobiletest', action='unfollow', format='json'),
+            params={'_authentication_token': self.auth_token},
+            status=400
+        )
 
