0607eaf242716d3498076829d89f46feb08f603f
Allan <a.callaghan@civicboom.com>
2010-12-15 16:09:36 +0000

    experiements with animation effects

--- src/civicboom/public/javascript/cb_frag.js
+++ src/civicboom/public/javascript/cb_frag.js
@@ -4,11 +4,13 @@
 
 // Example: <a href="http://localhost:5000/test/frag" onclick="cb_frag($(this), 'http://localhost:5000/test/frag'); return false;">Link of AJAX</a>
 
+var frag_id_name                      = 'frag_';
 var fragment_containers_id            = '#frag_containers';
 var fragment_container_class          = 'frag_container';
 var fragment_div_loading_placeholder = '<p>loading</p>';
 var scroll_duration = 1000;
 
+var frag_count      = 0;
 var frag_loading    = null;
 var frags_to_remove = null;
 
@@ -27,19 +29,14 @@ function cb_frag(current_element, url) {
 	// Get this parent fragment name
 	var frag_div    = current_element.parents('.'+fragment_container_class) // go up the chain looking for '.frag' class to id the master parent
 	var frag_div_id = frag_div.attr('id');
-
+	
 	// Flag elements after this one for removal
 	frags_to_remove = frag_div.nextAll();
-	frags_to_remove.remove();
-	frags_to_remove = null;
-	//frags_to_remove.fadeOut(500);
-	//if (frags_to_remove.length > 0) {
-	//	Y.log('scroll back frags '+ frags_to_remove.length);
-	//	$(fragment_containers_id).scrollTo(frag_div, {duration: scroll_duration});
-	//}
+	//frags_to_remove.animate({width: 'toggle', opacity: 'toggle'}, 500, function(){frags_to_remove.remove(); frags_to_remove = null;});
+	frags_to_remove.fadeOut(scroll_duration, function(){frags_to_remove.remove(); frags_to_remove = null;});
 	
 	// Generate new div name
-	var frag_div_id_next = frag_div_id+'1'; // AllanC - I know this only appends one to the end, but it works
+	var frag_div_id_next = frag_id_name + frag_count++; //frag_div_id+'1'; // AllanC - I know this only appends one to the end, but it works
 	
 	// Create new div with loading placeholder
 	$(fragment_containers_id).append('<div id="'+frag_div_id_next+'">'+fragment_div_loading_placeholder+'</div>'); // Append new '.frag' div populated with with load placeholder data
@@ -57,13 +54,10 @@ function scroll_fragment_containers_right() {
 	// Scroll (smoothly) to the right max
 	// http://plugins.jquery.com/project/ScrollTo
 	// http://demos.flesler.com/jquery/scrollTo/
-	if (frags_to_remove) {
-		Y.log('removing frags '+ frags_to_remove.length);
-		frags_to_remove.remove();
-		frags_to_remove = null;
-	}
 	if (frag_loading) {
 		frag_loading.hide();
+		//frag_loading.width(0);
+		//frag_loading.animate({width: 'toggle', opacity: 'toggle'}, scroll_duration);
 		frag_loading.fadeIn(scroll_duration);
 		frag_loading = null;
 	}
