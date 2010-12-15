c15740f9e72034126eb810dbb8549ae000e2cfbd
Allan <a.callaghan@civicboom.com>
2010-12-15 15:39:08 +0000

    Added smooth fading of new fragments Experiemented with smooth scrolling of removing components

--- src/civicboom/public/javascript/cb_frag.js
+++ src/civicboom/public/javascript/cb_frag.js
@@ -7,8 +7,10 @@
 var fragment_containers_id            = '#frag_containers';
 var fragment_container_class          = 'frag_container';
 var fragment_div_loading_placeholder = '<p>loading</p>';
-var scroll_duration = 400;
+var scroll_duration = 1000;
 
+var frag_loading    = null;
+var frags_to_remove = null;
 
 // current_element = JQuery element object
 // url             = String
@@ -26,14 +28,15 @@ function cb_frag(current_element, url) {
 	var frag_div    = current_element.parents('.'+fragment_container_class) // go up the chain looking for '.frag' class to id the master parent
 	var frag_div_id = frag_div.attr('id');
 
-	//
-	if (frag_div.next().length > 0) {
-		//Y.log('scroll back');
-		//$(fragment_containers_id).scrollTo(frag_div, {duration: scroll_duration});
-	}
-
-	// Remove all element inserted after this element
-	frag_div.nextAll().remove(); 
+	// Flag elements after this one for removal
+	frags_to_remove = frag_div.nextAll();
+	frags_to_remove.remove();
+	frags_to_remove = null;
+	//frags_to_remove.fadeOut(500);
+	//if (frags_to_remove.length > 0) {
+	//	Y.log('scroll back frags '+ frags_to_remove.length);
+	//	$(fragment_containers_id).scrollTo(frag_div, {duration: scroll_duration});
+	//}
 	
 	// Generate new div name
 	var frag_div_id_next = frag_div_id+'1'; // AllanC - I know this only appends one to the end, but it works
@@ -43,14 +46,26 @@ function cb_frag(current_element, url) {
 	var frag_div_next = $('#'+frag_div_id_next);
 	frag_div_next.addClass(fragment_container_class);
 	
+	frag_loading = frag_div_next;
+	$(fragment_containers_id).scrollTo('100%', 0 , {duration: scroll_duration});
+	
 	// AJAX load html fragment
 	frag_div_next.load(url, scroll_fragment_containers_right);
-	scroll_fragment_containers_right();
 }
 
-function scroll_fragment_containers_right() {
+function scroll_fragment_containers_right() {	
 	// Scroll (smoothly) to the right max
 	// http://plugins.jquery.com/project/ScrollTo
 	// http://demos.flesler.com/jquery/scrollTo/
+	if (frags_to_remove) {
+		Y.log('removing frags '+ frags_to_remove.length);
+		frags_to_remove.remove();
+		frags_to_remove = null;
+	}
+	if (frag_loading) {
+		frag_loading.hide();
+		frag_loading.fadeIn(scroll_duration);
+		frag_loading = null;
+	}
 	$(fragment_containers_id).scrollTo('100%', 0 , {duration: scroll_duration});
 }
