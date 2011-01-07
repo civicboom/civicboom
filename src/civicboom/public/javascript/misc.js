e512585905a8afaaf97f4edc624c9cf1e0b186e8
Shish <shish@civicboom.com>
2011-01-07 19:56:21 +0000

    nobody used this function any more; and if anyone wants the functionality, jQuery has it built in

--- src/civicboom/public/javascript/misc.js
+++ src/civicboom/public/javascript/misc.js
@@ -1,23 +1,5 @@
 // Misc Functions
 
-
-// References - http://stackoverflow.com/questions/247483/http-get-request-in-javascript
-//              http://www.jibbering.com/2002/4/httprequest.html
-function getHTML(url, get_complete_function) {
-    var xmlHttp = null;
-    function ProcessRequest() {
-        //YAHOO.log("got the request!! State:" + xmlHttp.readyState + " status:"+xmlHttp.status);
-        if (xmlHttp.readyState == 4 && xmlHttp.status == 200) {
-            get_complete_function(xmlHttp.responseText);
-            //head_complete_function(xmlHttp.getAllResponseHeaders());
-        }
-    }
-    xmlHttp = new XMLHttpRequest(); 
-    xmlHttp.onreadystatechange = ProcessRequest;
-    xmlHttp.open("GET", url, true);
-    xmlHttp.send(null);
-}
-
 // Requires toggle_div.js functions
 // MARKED FOR DEPRICATION
 function setSingleCSSClass(element_to_style, class_to_set, parent_id) {
