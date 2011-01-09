3568c635d2788393f46672efab36393d26f3185b
Shish <shish@civicboom.com>
2011-01-09 12:27:47 +0000

    split tags on all non-alphanumeric

--- src/convert_legacy.py
+++ src/convert_legacy.py
@@ -113,12 +113,13 @@ def convert_legacy_database(url): # pragma: no cover - this should only be run a
                 string_tags = string_tags + row["tag"] + " "
             if "Tags" in row:
                 string_tags = string_tags + row["Tags"] + " "
-            tags = [get_tag(n) for n in re.split("[, ]", string_tags)]
+            tags = [get_tag(n) for n in re.split("[^a-zA-Z0-9\-]", string_tags) if len(n) > 0]
 
             if "catId" in row:
                 a.tags.append(get_tag_by_old_category_id(row["catId"]))
             if "CatId" in row:
                 a.tags.append(get_tag_by_old_category_id(row["CatId"]))
+
             return list(set(tags))
 
         def get_content(row):
