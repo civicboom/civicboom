f7244245a58c9d8e060a1b0147d569ec9b2ed95f
Shish <shish@civicboom.com>
2011-01-09 19:17:35 +0000

    don't use wh_public yet -- can enable it if necessary later

--- src/civicboom/model/content.py
+++ src/civicboom/model/content.py
@@ -258,11 +258,11 @@ class Content(Base):
             if thumbnail_url and thumbnail_url!="":
                 return thumbnail_url
 
-        from civicboom.lib.helpers import wh_public
         thumbnail_type = self.__type__
         if thumbnail_type=='article' and self.approval != None:
             thumbnail_type = 'response'
-        return wh_public("images/default_thumbnail_%s.png" % thumbnail_type)
+
+        return "/images/default_thumbnail_%s.png" % thumbnail_type
 
     @property
     def url(self):
