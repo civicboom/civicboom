a6c4ff2b699ef200141966777c645c79bb2bb790
Allan <a.callaghan@civicboom.com>
2011-01-18 12:19:45 +0000

    added validator skipping for unique username if username held by current user

--- src/civicboom/lib/form_validators/registration.py
+++ src/civicboom/lib/form_validators/registration.py
@@ -48,6 +48,10 @@ class UniqueUsernameValidator(validators.FancyValidator):
             raise formencode.Invalid(self.message("too_few", state, min=self.min), value, state)
         if len(value) >= self.max:
             raise formencode.Invalid(self.message("too_long", state, max=self.max), value, state)
+            
+        from pylons import tmpl_context as c
+        if c.logged_in_persona and c.logged_in_persona.username == value: # If the current user has this username then bypass the validator
+            return value
         if Session.query(Member).filter(Member.username==value).count() > 0:
             raise formencode.Invalid(self.message("username_taken", state, name=value), value, state)
         return value
