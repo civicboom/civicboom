03abc0924892a3bf67daf257c185f4123473fc8d
Allan <a.callaghan@civicboom.com>
2011-01-24 18:22:18 +0000

    profanity musings

---
+++ src/civicboom/tests/functional/test_profanity.py
@@ -0,0 +1,10 @@
+from civicboom.tests import *
+
+class TestProfanityController(TestController):
+    """
+    Placeholder for profanity automated test
+    """
+
+    def test_profanity(self):
+        pass
+
