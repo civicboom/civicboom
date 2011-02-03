7b824f9d4a801e0a57d86e7d8e614f34f5e5f488
Allan <a.callaghan@civicboom.com>
2011-02-03 20:27:12 +0000

    fixed bug with pubishing content from mobile by auto setting 'publish' if type different

--- src/civicboom/controllers/contents.py
+++ src/civicboom/controllers/contents.py
@@ -425,6 +425,8 @@ class ContentsController(BaseController):
                 return submit_keys[0]
             raise action_error(_('multiple submit types submited'), code=400)
         submit_type = normalise_submit(kwargs)
+        if submit_type==None and content.__type__!= kwargs.get('type'):
+            submit_type == 'publish'
 
         # -- Validate ----------------------------------------------------------
 
