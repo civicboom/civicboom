77af44443343e072345802a43445ad74f469c86f
Shish <shish@civicboom.com>
2011-02-10 13:54:18 +0000

    mystery solved: we were checking for home_location then accessing location...

--- src/civicboom/controllers/settings.py
+++ src/civicboom/controllers/settings.py
@@ -257,12 +257,12 @@ class SettingsController(BaseController):
             try:
                 (lon, lat) = [float(n) for n in settings["home_location"].split(",")]
             except Exception, e:
-                user_log.exception("Unable to understand location '%s'" % str(settings["location"]))
-                raise action_error(_("Unable to understand location '%s'" % str(settings["location"])))
+                user_log.exception("Unable to understand location '%s'" % str(settings["home_location"]))
+                raise action_error(_("Unable to understand location '%s'" % str(settings["home_location"])))
             user.location = "SRID=4326;POINT(%d %d)" % (lon, lat)
             del settings['home_location']
         elif settings.get("home_location_name"):
-            (lon, lat) = (0, 0) # FIXME: guess_lon_lat_from_name(request.POST["location_name"]), see Feature #47
+            (lon, lat) = (0, 0) # FIXME: guess_lon_lat_from_name(request.POST["home_location_name"]), see Feature #47
             user.location = "SRID=4326;POINT(%d %d)" % (lon, lat)
             del settings['home_location_name']
 
