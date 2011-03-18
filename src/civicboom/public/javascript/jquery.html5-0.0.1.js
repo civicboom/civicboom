2765359ad7d1cedb4e00d081d5606dfce6eb7420
Shish <shish@civicboom.com>
2011-03-18 11:36:39 +0000

    prune old code

--- src/civicboom/public/javascript/jquery.html5-0.0.1.js
+++ src/civicboom/public/javascript/jquery.html5-0.0.1.js
@@ -1,35 +1,5 @@
 // HTML5 feature emulation for older browsers
 
-// $(function() {
-// 
-// // http://borderstylo.com/posts/213-html5-feature-detection-and-adding-support-for-the-placeholder-attribute
-// if (!Modernizr.input.placeholder){
-//   $('input').each(function(i){
-//     if ($(this).val() =='')
-//       $(this).val($(this).attr('placeholder'));
-// 
-//     $(this).focus(function(e){
-//       if ($(this).val() === $(this).attr('placeholder')) {
-//         $(this).val('');
-//       }
-//     });
-// 
-//     $(this).blur(function(e){
-//       if ($(this).val() === '') {
-//         $(this).val($(this).attr('placeholder'));
-//       }
-//     });
-//   });
-// 
-//   $('form').submit(function(e){
-//     $(this).find('input').each(function(i){
-//       if ($(this).val() === $(this).attr('placeholder')) {
-//         $(this).val('');
-//       }
-//     });
-//   });
-// }
-
 function html5ize (jqContainer) {
 	if (typeof jqContainer == 'undefined')
 		jqContainer = $('body')
