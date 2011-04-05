7e98b4e17af03dbc7047ed10fe38cd63cbe55ed7
Shish <shish@civicboom.com>
2011-04-05 15:50:24 +0100

    start of 'do something when r=qr', see issue #451

--- src/civicboom/public/javascript/mobile.js
+++ src/civicboom/public/javascript/mobile.js
@@ -4,3 +4,16 @@ if(navigator.platform == 'iPad' || navigator.platform == 'iPhone' || navigator.p
 	//$("footer").css("position", "static");
 	//$("#app").css("width", "100%").css("height", "100%").css("overflow", "scroll");
 };
+
+var r = $(document).getUrlParam("r");
+/*
+if(r) {
+	$.cookie('r', r, { expires: new Date((new Date()).getTime() + 60*60*1000), path: '/' });
+}
+*/
+if(r && r == "qr") {
+	var uagent = navigator.userAgent.toLowerCase();
+	if(uagent.search("android") > -1) {
+		var url = "market://details?id=com.civicboom.mobile2";
+	}
+}
