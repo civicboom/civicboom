44e3c6bd767faa5eb5d4ad243c64d7fe1b19c9f3
Shish <shish@civicboom.com>
2011-04-13 13:57:10 +0100

    beaker fills the cache with loads of session things; put that in a separate db for easier debugging

--- src/civicboom/lib/redis_.py
+++ src/civicboom/lib/redis_.py
@@ -86,7 +86,7 @@ class RedisManager(NoSqlManager):
         NoSqlManager.__init__(self, namespace, url=url, data_dir=data_dir, lock_dir=lock_dir, **params)
 
     def open_connection(self, host, port, **params):
-        self.db_conn = Redis(host=host, port=int(port), **params)
+        self.db_conn = Redis(host=host, port=int(port), db=1, **params)
 
     def __contains__(self, key):
         log.debug('%s contained in redis cache (as %s) : %s'%(key, self._format_key(key), self.db_conn.exists(self._format_key(key))))
