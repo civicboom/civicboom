b1b7be488419409291a0408575eba9ef52c333a7
Shish <shish@civicboom.com>
2011-04-13 14:44:47 +0100

    move from in-memory worker queue to a redis-based one (still one queue per node though, as media processing needs to happen on the same node as upload)

--- src/civicboom/lib/worker.py
+++ src/civicboom/lib/worker.py
@@ -6,13 +6,15 @@ http://chrismoos.com/2009/03/04/pylons-worker-threads/
 from Queue import Queue
 from threading import Thread
 from time import sleep
+import redis
+import json
 
 
 import logging
 log = logging.getLogger(__name__)
 
 _workers           = []
-_worker_queue      = Queue()
+_worker_queue      = None
 _process_functions = {}
 
 
@@ -26,32 +28,61 @@ add_process_function('send_message' , send_message )
 
 
 
+class RedisQueue(object):
+    """An abstract FIFO queue"""
+    def __init__(self, redis, queue_id=None):
+        self.r = redis
+        self.queue_id = "queue:%s" % (queue_id or self.r.incr("queue_space"))
+
+    def put(self, element):
+        """Push an element to the tail of the queue"""
+        self.r.rpush(self.queue_id, json.dumps(element))
+
+    def get(self):
+        """Pop an element from the head of the queue"""
+        return json.loads(self.r.blpop(self.queue_id)[1])
+
+    def qsize(self):
+        return self.r.llen(self.queue_id)
+
+    def task_done(self):
+        pass
+
+
 class MediaThread(Thread):
     def run(self):
         log.info('Media processing thread is running.')
         live = True
 
         while live:
-            task = _worker_queue.get()
             try:
-                task_type = task.pop("task")
-                log.info('Starting task: %s (%s) [approx %d left]' % (task_type, task, _worker_queue.qsize()))
-                if task_type in _process_functions:
-                    _process_functions[task_type](**task)
-                #if task_type == "process_media":
-                #    process_media(**task)
-                #if task_type == "send_message":
-                #    send_message(**task)
-                if task_type == "die":
-                    live = False
-            except Exception as e:
-                log.exception('Error in worker thread:')
-            _worker_queue.task_done()
-            sleep(3)
+                task = _worker_queue.get()
+                try:
+                    task_type = task.pop("task")
+                    log.info('Starting task: %s (%s) [approx %d left]' % (task_type, task, _worker_queue.qsize()))
+                    if task_type in _process_functions:
+                        _process_functions[task_type](**task)
+                    #if task_type == "process_media":
+                    #    process_media(**task)
+                    #if task_type == "send_message":
+                    #    send_message(**task)
+                    if task_type == "die":
+                        live = False
+                except Exception as e:
+                    log.exception('Error in worker thread:')
+                    sleep(3)
+                _worker_queue.task_done()
+            except redis.exceptions.ConnectionError:
+                log.error("Redis server went away while waiting for task; sleeping before reconnect")
+                sleep(3)
 
 
 def start_worker(count=3):
+    global _worker_queue
     log.info('Starting worker threads.')
+    from pylons import app_globals
+    import platform
+    _worker_queue = RedisQueue(app_globals.memcache, platform.node())
     for n in range(count):
         worker = MediaThread()
         worker.daemon = True
