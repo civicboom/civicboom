7f0a2923218d8c0f79df464183e814a10bfcb4b0
Shish <shish@civicboom.com>
2011-06-02 15:47:35 +0100

    more routes testing

--- src/civicboom/tests/test_routes.py
+++ src/civicboom/tests/test_routes.py
@@ -6,40 +6,71 @@ from civicboom.lib.web import url
 class TestRoutes(TestController):
     def test_proto(self):
         self.assertEqual(
-            url(protocol="http", controller='contents', action='show', id=123),
+            url('content', id=123, protocol="http"),
             "http://www.civicboom.com/contents/123"
         )
-        self.assertEqual(  # same protocol = do have full URL?
-            url(protocol="https", controller='contents', action='show', id=123),
+        self.assertEqual(
+            url('content', id=123, protocol="https"),
             "https://www.civicboom.com/contents/123"
         )
 
     def test_subdomain(self):
         self.assertEqual(  # different subdomain = do have full URL
-            url(sub_domain="m", controller='contents', action='show', id=123),
+            url('content', id=123, sub_domain="m"),
             "https://m.civicboom.com/contents/123"
         )
         self.assertEqual(  # same subdomain = don't have full URL
-            url(sub_domain="www", controller='contents', action='show', id=123),
+            url('content', id=123, sub_domain="www"),
             "/contents/123"
         )
         self.assertEqual(  # unless qualified=True
-            url(sub_domain="www", controller='contents', action='show', id=123, qualified=True),
+            url('content', id=123, sub_domain="www", qualified=True),
             "https://www.civicboom.com/contents/123"
         )
 
     def test_host(self):
+        # setting hosts is broken weirdly
         self.assertEqual(
-            url(host="pie.civicboom.com", controller='contents', action='show', id=123),
+            url('content', id=123, host="pie.civicboom.com"),
             "https://pie.civicboom.com/contents/123"
         )
         self.assertEqual(
-            url(host="www.civicboom.com", controller='contents', action='show', id=123),
+            url('content', id=123, host="www.civicboom.com"),
             "https://www.civicboom.com/contents/123"
         )
         self.assertEqual(
-            url(host="www.civicboom.com", controller='contents', action='show', id=123, qualified=True),
+            url('content', id=123, host="www.civicboom.com", qualified=True, sub_domain="www"),
             "https://www.civicboom.com/contents/123"
+        )
+
+    def test_rest_routes(self):
+        self.assertEqual(
+            url('content', id=123),
+            "/contents/123"
+        )
+        self.assertEqual(
+            url('edit_content', id=123),
+            "/contents/123/edit"
+        )
+        self.assertEqual(
+            url('edit_content', id=123, format="json"),
+            "/contents/123/edit.json"
+        )
+        self.assertEqual(
+            url('contents'),
+            "/contents"
+        )
+        self.assertEqual(
+            url('new_content'),
+            "/contents/new"
+        )
+        self.assertEqual(
+            url('member', id='shish'),
+            "/members/shish"
+        )
+        self.assertEqual(
+            url('member_action', id='shish', action='follow'),
+            "/members/shish/follow"
         )
 
     def test_current(self):
@@ -50,16 +81,6 @@ class TestRoutes(TestController):
         self.assertEqual(
             url('current', qualified=True),
             "https://www.civicboom.com/"
-        )
-
-    def test_rest_routes(self):
-        self.assertEqual(
-            url(controller='contents', action='show', id=123),
-            "/contents/123"
-        )
-        self.assertEqual(
-            url(controller='contents', action='index'),
-            "/contents"
         )
 
     def test_custom_routes(self):
@@ -106,24 +127,3 @@ class TestRoutes(TestController):
             "/mobile/media_init.json"
         )
 
-    def test_rest_formats(self):
-        self.assertEqual(
-            url(controller='members', action="show", id='shish'),
-            "/members/shish"
-        )
-
-        self.assertEqual(
-            url('member', id='shish'),
-            "/members/shish"
-        )
-
-        self.assertEqual(
-            url('member_action', id='shish', action='follow'),
-            "/members/shish/follow.html"
-        )
-
-        # with a specified format, it should be .format
-        self.assertEquals(
-            url('member_action', id='shish', action='follow', format='json'),
-            "/members/shish/follow.json"
-        )
