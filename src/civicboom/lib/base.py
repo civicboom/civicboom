fbf7753f0d617ad2f44247c50c0cf523b89ab392
Shish <shish@civicboom.com>
2011-06-15 17:09:13 +0100

    civicboom_lib depends on base for lots of things; base depends on civicboom_lib for deny_pending_user. Import the latter on the fly

--- src/civicboom/lib/base.py
+++ src/civicboom/lib/base.py
@@ -21,7 +21,6 @@ from civicboom.lib.web                 import url, redirect, redirect_to_referer
 from civicboom.lib.database.get_cached import get_member as _get_member, get_group as _get_group, get_membership as _get_membership, get_membership_tree as _get_membership_tree, get_message as _get_message, get_content as _get_content, get_members
 from civicboom.lib.database.etag_manager import gen_cache_key
 from civicboom.lib.database.query_helpers import to_apilist
-from civicboom.lib.civicboom_lib       import deny_pending_user
 from civicboom.lib.authentication      import authorize, get_lowest_role_for_user
 from civicboom.lib.permissions         import account_type, role_required, age_required, has_role_required, raise_if_current_role_insufficent
 
@@ -409,6 +408,7 @@ class BaseController(WSGIController):
         
         # User pending regisration? --------------------------------------------
         # redirect to complete registration process
+        from civicboom.lib.civicboom_lib import deny_pending_user  # import late to avoid dependency loop
         if c.logged_in_user and c.logged_in_user.status=='pending' and deny_pending_user(url('current')):
             set_flash_message(_('Please complete the registration process'))
             redirect(url(controller='register', action='new_user', id=c.logged_in_user.id))
