65c0741fd13144e04dbddf53c5ea6ae596125c00
Shish <shish@civicboom.com>
2011-06-15 16:21:50 +0100

    _() needs to be called in realtime - calling it when the module is first imported breaks things as no translator has been registered at module import time

--- src/civicboom/lib/constants.py
+++ src/civicboom/lib/constants.py
@@ -7,19 +7,19 @@ from pylons.i18n.translation  import _
 
 import re
 
-# in the form list_name, icon, display_text
-contents_list_titles = [
-    #list name , icon, description
-    ('all'                 , 'article'    , _('all').capitalize()                   ),
-    ('drafts'              , 'draft'      , _('drafts').capitalize()                ),
-    ('assignments_active'  , 'assignment' , _('active _assignments').capitalize()   ),
-    ('assignments_previous', 'assignment' , _('previous _assignments').capitalize() ),
-    ('responses'           , 'response'   , _('responses').capitalize()             ),
-    ('articles'            , 'article'    , _('_articles').capitalize()             ),
-]
-
 
 def get_list_titles(list_name):
+    # in the form list_name, icon, display_text
+    contents_list_titles = [
+        #list name , icon, description
+        ('all'                 , 'article'    , _('all').capitalize()                   ),
+        ('drafts'              , 'draft'      , _('drafts').capitalize()                ),
+        ('assignments_active'  , 'assignment' , _('active _assignments').capitalize()   ),
+        ('assignments_previous', 'assignment' , _('previous _assignments').capitalize() ),
+        ('responses'           , 'response'   , _('responses').capitalize()             ),
+        ('articles'            , 'article'    , _('_articles').capitalize()             ),
+    ]
+
     for (list, icon, description) in contents_list_titles:
         if list == list_name:
             return (icon, description)
