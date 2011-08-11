bb7282395abdcc8220f4a64c2828758c3819e860
Allan <a.callaghan@civicboom.com>
2011-08-11 18:21:31 +0100

    cleanup unneeded files

--- src/civicboom/lib/app_globals.py
+++ src/civicboom/lib/app_globals.py
@@ -1,8 +1,5 @@
 """The application's Globals object"""
 
-#from beaker.cache import CacheManager
-#from beaker.util import parse_cache_config_options
-#from paste.deploy.converters import asbool
 
 import redis
 import os
@@ -20,13 +17,11 @@ class Globals(object):
         initialization and is available during requests via the
         'app_globals' variable
         """
-
+    
         if os.path.exists(".version"):
             self.version   = file(".version").read().strip()
         else:  # pragma: no cover - all released versions have a version
             self.version   = None
         
-        #self.cache         = CacheManager(**parse_cache_config_options(config))
-        #self.cache_enabled = asbool(config['beaker.cache.enabled']) # Also used by lib.database
-
+        
         self.memcache      = redis.Redis(config['service.redis.server'])
