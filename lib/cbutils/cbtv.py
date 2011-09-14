39256a6c455ecbae7de9fda77b275d91ccdec4cf
Shish <shish@civicboom.com>
2011-09-14 13:17:31 +0100

    note of future feature

--- lib/cbutils/cbtv.py
+++ lib/cbutils/cbtv.py
@@ -4,6 +4,8 @@
 # full-file navigation
 # - cbtv_events logs can last for hours, but only a minute at a time is
 #   sensibly viewable
+# mark lock claim / release
+# - seeing what is locking software is good
 
 from __future__ import print_function
 from decorator import decorator
