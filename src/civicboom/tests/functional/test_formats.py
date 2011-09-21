8c5416cf1394817fc7b6f932531346bd745ace31
Allan <a.callaghan@civicboom.com>
2011-09-21 15:33:16 +0100

    Added a few more automated tests to test 'message index templates'

--- src/civicboom/tests/functional/test_formats.py
+++ src/civicboom/tests/functional/test_formats.py
@@ -15,18 +15,28 @@ class TestFormats(TestController):
                 self.assertIn('API Documentation', response)
                 response = self.app.get(url('member' , id='unittest', format=format), status=200)
                 self.assertIn('unittest'         , response)
+                if self.logged_in_as:
+                    response = self.app.get(url('messages', list='notification', format=format), status=200)
+                    self.assertIn('Base Notification', response)
         
         test_all_formats() # Logged in user
         self.log_out()
         test_all_formats() # Anon user
 
     def test_sub_domains(self):
+        
         def test_all_sub_domains():
             for sub_domain in ['mobile', 'widget']:
+                # Check member template rendering
                 response = self.app.get(url('content', id=1         ), extra_environ={'HTTP_HOST': '%s.civicboom_test.com' % sub_domain}, status=200)
                 self.assertIn('API Documentation', response)
+                # Check content template rendering
                 response = self.app.get(url('member' , id='unittest'), extra_environ={'HTTP_HOST': '%s.civicboom_test.com' % sub_domain}, status=200)
                 self.assertIn('unittest'         , response)
+                if self.logged_in_as and sub_domain!='widget':
+                    # Check messages template
+                    response = self.app.get(url('messages'), extra_environ={'HTTP_HOST': '%s.civicboom_test.com' % sub_domain}, status=200)
+                    self.assertIn('Base Notification', response)
         
         test_all_sub_domains() # Logged in user
         self.log_out()
