80682e57d75fc6f35dff6a3fb05682c1599397a1
Shish <shish@civicboom.com>
2011-10-25 17:57:26 +0100

    escapes in docstrings

--- lib/cbutils/text.py
+++ lib/cbutils/text.py
@@ -150,9 +150,8 @@ def strip_html_tags(text):
     
     >>> strip_html_tags(u'<h1>Test</h1>')
     u'Test'
-    >>> strip_html_tags(u'<h1>Test</h1>\n<p style="no">clean</p>\n\n<br/><a href="#">me</a>')
-    u'Test \n clean \n\n me'
-    
+    >>> strip_html_tags(u'<h1>Test</h1>\\n<p style="no">clean</p>\\n\\n<br/><a href="#">me</a>')
+    u'Test \\n clean \\n\\n me'
     """
     #import webhelpers.markdown.HtmlBlockPreprocessor - using proper modules to do this would be good, couldnt work out how to use this HTML processor
     #text = webhelpers.html.converters.markdown(text)
