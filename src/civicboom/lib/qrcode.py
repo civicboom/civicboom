6746070cfb58ced6011e524c254e5a0c1e4767f3
Allan <a.callaghan@civicboom.com>
2011-11-04 15:30:14 +0000

    QRCode error level param

--- src/civicboom/lib/qrcode.py
+++ src/civicboom/lib/qrcode.py
@@ -2,9 +2,9 @@ import qrencode
 import StringIO
 
 
-def qrcode_stream(text, size=100, format='png'):
+def qrcode_stream(text, size=210, format='png', **kwargs):
     buf = StringIO.StringIO()
-    qrencode.encode_scaled(text, size)[2].save(buf, format=format)
+    qrencode.encode_scaled(text, size, **kwargs)[2].save(buf, format=format)
     return buf.getvalue()
 
 
@@ -12,8 +12,10 @@ from pylons import tmpl_context as c, response
 from civicboom.lib.web import action_error
 
 def cb_qrcode(cb_url, **kwargs):
-    kwargs['format'] = c.format if c.format in ['png', 'jpeg', 'gif', 'bmp', 'tiff'] else 'png'
-    kwargs['size'  ] = int(kwargs.get('size', 200))
+    kwargs['format'        ] = c.format if c.format in ['png', 'jpeg', 'gif', 'bmp', 'tiff'] else 'png'
+    kwargs['size'          ] = int(kwargs.get('size', 210))
+    kwargs['level'         ] = int(kwargs.get('level', qrencode.QR_ECLEVEL_M))
+    #kwargs['case_sensitive'] = kwargs.get('case_sensitive', False)
     try:
         qr_stream = qrcode_stream(cb_url, **kwargs)
         response.headers['Content-type'] = "image/%s" % kwargs['format']
